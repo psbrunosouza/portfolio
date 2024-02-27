f37e6b9021c0c44366a59631ea67dfe768c49a1c
psbrunosouza <psbrunosouza@gmail.com>
2024-02-27 14:32:33 -0300

    feat: improve theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,36 @@ export default {
   theme: {
     extend: {
       colors: {
-        'base': 'var(--base)',
-        'accent': 'var(--accent)',
-        'highlight': 'var(--highlight)',
-        light: 'var(--light)',
-        dark: 'var(--dark)',
+        'base': {
+          DEFAULT: 'var(--base-200)',
+          '100': 'var(--base-100)',
+          '200': 'var(--base-200)',
+          '300': 'var(--base-300)',
+        },
+        'accent': {
+          'DEFAULT': 'var(--accent-200)',
+          '100': 'var(--accent-100)',
+          '200': 'var(--accent-200)',
+          '300': 'var(--accent-300)',
+        },
+        'highlight': {
+          'DEFAULT': 'var(--highlight-200)',
+          '100': 'var(--highlight-100)',
+          '200': 'var(--highlight-200)',
+          '300': 'var(--highlight-300)',
+        },
+        light: {
+          'DEFAULT': 'var(--light-200)',
+          '100': 'var(--light-100)',
+          '200': 'var(--light-200)',
+          '300': 'var(--light-300)',
+        },
+        dark: {
+          'DEFAULT': 'var(--dark-200)',
+          '100': 'var(--dark-100)',
+          '200': 'var(--dark-200)',
+          '300': 'var(--dark-300)',
+        },
       },
        fontFamily: {
         'open-sans-regular': 'var(--font-regular)',
