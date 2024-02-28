74b3095ceeb97ec92795a1447d679ccded3ebf5d
psbrunosouza <psbrunosouza@gmail.com>
2024-02-28 09:01:34 -0300

    feat: implement projects

--- src/profile/profile.ts
+++ src/profile/profile.ts
@@ -1,12 +1,25 @@
 import "./profile.css";
 import template from "./profile.html?raw";
 
-const getDescriptions = (subtitle: HTMLHeadElement) => {
-  const subtitles = [
-    "A experienced Software Developer, crafting beautiful web experiences.",
-  ];
-  subtitle.innerHTML = subtitles[Math.floor(Math.random() * subtitles.length)];
+const setProficiencies = (proficiencyRef: HTMLDivElement) => {
+  [
+    { name: "Javascript" },
+    { name: "Typescript" },
+    { name: "Golang" },
+    { name: "Angular" },
+  ].forEach((proficiency) => {
+    proficiencyRef.innerHTML += `<p class="p-2 bg-accent">${proficiency.name}</p>`;
+  });
 };
 
+export const setActiveMenu = (id: string) => {
+  document.querySelector<HTMLLIElement>(".active")?.classList.remove("active");
+  const menuItem = document.querySelector<HTMLLIElement>("#" + id);
+  menuItem?.classList.add("active");
+};
+
+// @ts-ignore
+window.setActiveMenu = setActiveMenu;
+
 document.querySelector<HTMLDivElement>("#content")!.innerHTML = template;
-getDescriptions(document.querySelector<HTMLHeadElement>("#subtitle")!);
+setProficiencies(document.querySelector<HTMLDivElement>("#proficiencies")!);
